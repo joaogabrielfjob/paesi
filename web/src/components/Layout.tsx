fed7e2f35ece3342ffdc503a9a3b4cdf902b2d32
João Job <joao.gabrielfjob@gmail.com>
2025-10-06 14:21:58 -0300

    fix: add scroll restoration

--- web/src/components/Layout.tsx
+++ web/src/components/Layout.tsx
@@ -1,4 +1,4 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, ScrollRestoration } from 'react-router-dom';
 import { Header } from './Header';
 
 export function Layout() {
@@ -6,6 +6,7 @@ export function Layout() {
     <div className='z-[1] flex flex-grow flex-col'>
       <Header />
       <Outlet />
+      <ScrollRestoration />
     </div>
   );
 }
