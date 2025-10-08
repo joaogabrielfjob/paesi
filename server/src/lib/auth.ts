db057249cd46ecf87027ecd7bb5ca34b7b84de92
João Job <joao.gabrielfjob@gmail.com>
2025-10-08 02:14:03 -0300

    chore: add cookie config to better auth

--- server/src/lib/auth.ts
+++ server/src/lib/auth.ts
@@ -26,4 +26,11 @@ export const auth = betterAuth({
       }
     }
   },
+  advanced: {
+    defaultCookieAttributes: {
+      sameSite: 'none',
+      secure: true,
+      partitioned: true
+    }
+  }
 })
